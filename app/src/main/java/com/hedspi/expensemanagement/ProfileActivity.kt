9215aefb7f9085e7438500d171838191fe132c8a
bekeodangyeuqn <thaiduiqn@gmail.com>
2024-01-25 23:39:02 +0700

    handle profile

--- app/src/main/java/com/hedspi/expensemanagement/ProfileActivity.kt
+++ app/src/main/java/com/hedspi/expensemanagement/ProfileActivity.kt
@@ -3,20 +3,58 @@ package com.hedspi.expensemanagement
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.room.Room
+import com.google.firebase.auth.FirebaseAuth
 import com.hedspi.expensemanagement.databinding.ActivityMainBinding
 import com.hedspi.expensemanagement.databinding.ActivityProfileBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var db: AppDatabase
+    private lateinit var user: User
+    private val auth = FirebaseAuth.getInstance()
+    private val userId = auth.currentUser?.uid
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        val usernameLayout = binding.profileUsername
+        val emailLayout = binding.profileMail
+        val moneyLayout = binding.profileMoney
+        val logoutBtn = binding.logoutBtn
+        val activity = this
+
         val bottomNavigationView = binding.bottomNavView
 
-        bottomNavigationView.selectedItemId = com.hedspi.expensemanagement.R.id.item_profile
+        bottomNavigationView.selectedItemId = R.id.item_profile
+
+        GlobalScope.launch {
+            db = Room.databaseBuilder(activity,
+                AppDatabase::class.java,
+                "transactions")
+                .addMigrations(migration_1_2)
+                .addMigrations(migration_2_3)
+                .addMigrations(migration_3_4)
+                .addMigrations(migration_4_5)
+                .build()
+
+            user = db.userDao().getUserById(userId!!)
+            val money = db.userDao().getMoneyByUserId(userId!!)
+
+            runOnUiThread {
+                usernameLayout.text = user.username
+                emailLayout.text = user.email
+                moneyLayout.text = money.toString()
+            }
+        }
+
+        logoutBtn.setOnClickListener { onLogoutButtonClick(it) }
 
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -37,5 +75,21 @@ class ProfileActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun onLogoutButtonClick(view: View) {
+        AlertDialog.Builder(this)
+            .setTitle("Logout")
+            .setMessage("Are you sure you want to log out?")
+            .setPositiveButton("Yes") { dialog, _ ->
+                auth.signOut()
+                startActivity(Intent(this, SignInActivity::class.java))
+                dialog.dismiss()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
     }
 }
